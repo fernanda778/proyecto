a8ac50184b1338d79050c965fd7d9e3908f57b81
fernanda778 <dfherrera067@gmail.com>
2025-05-29 21:11:23 -0600

    Delete index2.js

--- index2.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("regresar").addEventListener("click", function (event) {
-    event.preventDefault(); 
-    window.location.href = "index.html"; 
-});
-document.getElementById("reiniciar").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    location.reload(); 
-});
